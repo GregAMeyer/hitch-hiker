3c31c926c0465066401c30614d905ae452d211b6
Gregory Meyer <gregorymeyer@gregorys-macbook-pro.local>
2018-10-07 15:08:38 -0400

    Initial Commit

---
+++ hitch-hiker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hitch-hiker
+//
+//  Created by Gregory Meyer on 10/7/18.
+//  Copyright © 2018 Gregory Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
